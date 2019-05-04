d59cba7ba03b5ea0da8947772113cf2ed9b6a59b
愚道 <tingzhao.ytz@antfin.com>
2019-05-04 10:55:11 +0800

    bump 0.1.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 const fs = require('fs');
-const { join } = require('fs');
+const { join } = require('path');
 
 const cwd = process.cwd();
 const blocks = fs.readdirSync(cwd).filter(name => {
@@ -9,7 +10,13 @@ const blocks = fs.readdirSync(cwd).filter(name => {
 blocks.forEach(name => {
   const packagePath = join(cwd, name, 'package.json');
   if (fs.existsSync(packagePath)) {
-    const content = fs.readFileSync(packagePath);
-    console.log(content);
+    const content = require(packagePath);
+    content.blockConfig = {
+      specVersion: '0.1',
+    };
+    console.log(`start write blockConfig to ${packagePath}.`);
+    fs.writeFileSync(packagePath, JSON.stringify(content, null, 2), 'utf-8');
   }
 });
+
+console.log('done!');
